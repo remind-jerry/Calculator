46e1372311723a3ac448e0c50a0892f4271c93a9
remind-jerry <76900235+remind-jerry@users.noreply.github.com>
2021-01-30 17:16:54 +0400

    all buttons are ready

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -17,13 +17,28 @@ final class ViewController: UIViewController {
     @IBOutlet private weak var buttonNotUsed1: UIButton!
     @IBOutlet private weak var buttonNotUsed2: UIButton!
     @IBOutlet private weak var buttonSlash: UIButton!
+    @IBOutlet private weak var button7: UIButton!
+    @IBOutlet private weak var button8: UIButton!
+    @IBOutlet private weak var button9: UIButton!
+    @IBOutlet private weak var buttonX: UIButton!
+    @IBOutlet private weak var button4: UIButton!
+    @IBOutlet private weak var button5: UIButton!
+    @IBOutlet private weak var button6: UIButton!
+    @IBOutlet private weak var buttonMinus: UIButton!
+    @IBOutlet private weak var button1: UIButton!
+    @IBOutlet private weak var button2: UIButton!
+    @IBOutlet private weak var button3: UIButton!
+    @IBOutlet private weak var buttonPlus: UIButton!
+    @IBOutlet private weak var button0: UIButton!
+    @IBOutlet private weak var buttonNotUsed3: UIButton!
+    @IBOutlet private weak var buttonRavno: UIButton!
     
     // MARK: - Private properties
     
-    private var numberOfScreen:Double = 0;
-    private var firstNum:Double = 0;
-    private var mathSign:Bool = false;
-    private var operation:Int = 0;
+    private var numberOfScreen: Double = 0
+    private var firstNum: Double = 0
+    private var mathSign: Bool = false
+    private var operation: Int = 0
 
     // MARK: - Lifecycle
     
@@ -35,7 +50,7 @@ final class ViewController: UIViewController {
     // MARK: - Private methods
 
     private func setupButtonsCorners() {
-        [buttonC, buttonNotUsed1, buttonNotUsed2, buttonSlash].forEach {
+        [buttonC, buttonNotUsed1, buttonNotUsed2, buttonSlash, button1, button2, button3, button4, button5, button6,button7, button8, button9, button0, buttonX, buttonMinus, buttonPlus, buttonRavno, buttonNotUsed3].forEach {
             $0?.layer.cornerRadius = 15
         }
     }
@@ -46,8 +61,7 @@ final class ViewController: UIViewController {
         if mathSign == true {
             result.text = String(sender.tag)
             mathSign = false
-        }
-        else {
+        } else {
             result.text = result.text! + String(sender.tag)
         }
         
@@ -60,14 +74,11 @@ final class ViewController: UIViewController {
             firstNum = Double(result.text!)!
             if sender.tag == 11 { // \
                 result.text = "/";
-            }
-            else if sender.tag == 12 { // x
+            } else if sender.tag == 12 { // x
                 result.text = "x";
-            }
-            else if sender.tag == 13 { // -
+            } else if sender.tag == 13 { // -
                 result.text = "-";
-            }
-            else if sender.tag == 14 { // +
+            } else if sender.tag == 14 { // +
                 result.text = "+";
             }
             operation = sender.tag
